29d9bab14bb308073c070962064df8f83a78e56e
parkjh2038 <pjh950312@naver.com>
2018-09-18 16:35:10 +0900

    [2018-09-18] init inteface

--- app/src/main/java/com/example/a507/calculator/MainActivity.java
+++ app/src/main/java/com/example/a507/calculator/MainActivity.java
@@ -4,10 +4,28 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    protected double ans = 0.;
+
+    protected void inputNum() {
+
+    }
+
+    double op4(double x) {
+        return 0.;
+    }
+
+    double mathOp(double x) {
+        return 0.;
+    }
+
+    void printNum(double x) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("계산기");
     }
 }
